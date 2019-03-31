3b3f34242e8d165fb8f3eab2b2bdf49e6e110a83
deletestvn <deletestvn@gmail.com>
2019-03-31 01:15:49 -0400

    added FirefighterMovement to control movement, FirefighterManager to manage different actions and represent a firefighter, aGame(GameManager) to manage game in main scene. If the GameManager script existed is abandoned, this will replace it

---
+++ Flashpoint/Assets/Scripts/Firefighter/FirefighterManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FirefighterManager : MonoBehaviour
+{
+    //public Transform m_SpawnPoint;                          // The position and direction the firefighter will have when it spawns
+    //public int m_PlayerNumber;                              // This specifies which player this firefighter belongs to
+    //public GameObject m_Instance;                           // A reference to the instance of the firefighter when it is created
+
+    private int AP;                                         // The action points firefighter has    
+    private bool myTurn;                                    // This specifies if it is this firefighter's turn, controlled by FirefighterManager
+    private bool isCarryingVictim;
+    private bool isMoving;
+    private FirefighterMovement m_Movement;                 // Reference to firefighter's movement script, used to disable and enable control
+
+    public void Awake ()
+    {
+        m_Movement = GetComponent<FirefighterMovement> ();
+
+        isMoving = false;
+
+        //m_Movement.m_PlayerNumber = m_PlayerNumber;
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //m_Movement.Move();
+    if (isMoving)
+         {
+             m_Movement.Move();
+
+             if(Vector3.Distance(m_Movement.get_m_Transform().position, m_Movement.get_m_Target()) == 0)
+             {
+                 isMoving = false;
+                 m_Movement.get_m_Animator().SetBool("Move", false);
+             }
+         }
+    }
+
+    public void EnableMove()
+    {
+        isMoving = true;
+    }
+
+    public void DisableControl()
+    {
+        m_Movement.enabled = false;
+    }
+
+    public void EnableControl()
+    {
+        m_Movement.enabled = true; ;
+    }
+
+    public void SetTargetTile(Selectable TargetTile)
+    {
+        m_Movement.SetTarget(TargetTile);
+    }
+
+    public void Reset()
+    {
+
+    }
+
+
+
+}
---
+++ Flashpoint/Assets/Scripts/Firefighter/FirefighterMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirefighterMovement : MonoBehaviour
+{
+    //public int m_PlayerNumber = 1;        // Used to identify which firefighter belongs to which player. Set by firefighter's manager
+    public float m_Speed = 2.0f;            // Move speed
+    public float m_TurnSpeed = 180f;        // Rotate speed
+
+    private Transform m_Transform;          // Reference used to move the firefighter
+    private Vector3 m_Target;               // Reference of the target location
+    private Animator m_Animator;            // Reference used to change animation
+
+    private Selectable m_TargetTile;        // Reference of the selected tile
+
+    // Get methods
+    public Transform get_m_Transform() { return this.m_Transform; }
+    public Vector3 get_m_Target() { return this.m_Target; }
+    public Animator get_m_Animator() { return this.m_Animator; }
+
+    private void Awake()
+    {
+        m_Transform = GetComponent<Transform>();
+        m_Animator = GetComponent<Animator>();
+    }
+
+    public void SetTarget(Selectable TargetTile)
+    {
+        this.m_TargetTile = TargetTile;
+        this.m_Target = m_TargetTile.transform.position + new Vector3(0, 1, 0);
+    }
+
+    public void Move()
+    {
+        m_Animator.SetBool("Move", true);
+        m_Transform.position = Vector3.MoveTowards(m_Transform.position, m_Target, m_Speed * Time.deltaTime);
+    }
+
+    public void Turn(float turnInput)
+    {
+        Quaternion turnRotation = Quaternion.Euler(0f, turnInput, 0f);
+        m_Transform.rotation = Quaternion.Lerp(m_Transform.rotation, turnRotation, 3 * Time.deltaTime);
+    }
+}
