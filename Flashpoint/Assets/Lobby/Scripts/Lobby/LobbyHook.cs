15fd89238b207df33da28218e843dfc7d266f59d
Isabelle del Mundo <isabelledelmundo@gmail.com>
2019-04-07 15:26:21 -0400

    organized all unnecessary files

--- Flashpoint/Assets/Lobby/Scripts/Lobby/LobbyHook.cs
+++ Flashpoint/Assets/Lobby/Scripts/Lobby/LobbyHook.cs
@@ -8,15 +8,15 @@ namespace Prototype.NetworkLobby
 {
     // Subclass this and redefine the function you want
     // then add it to the lobby prefab
-    public class LobbyHook : MonoBehaviour
+    public abstract class LobbyHook : MonoBehaviour
     {
         public virtual void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer) {
-            //gamePlayer.GetComponent<Renderer>().material.color = lobbyPlayer.GetComponent<LobbyPlayer>().playerColor;
-            LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
-            SetupLocalPlayer localPlayer = gamePlayer.GetComponent<SetupLocalPlayer>();
+            // //gamePlayer.GetComponent<Renderer>().material.color = lobbyPlayer.GetComponent<LobbyPlayer>().playerColor;
+            // LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+            // SetupLocalPlayer localPlayer = gamePlayer.GetComponent<SetupLocalPlayer>();
 
-            localPlayer.pname = lobby.playerName;
-            localPlayer.playerColor = lobby.playerColor;
+            // localPlayer.pname = lobby.playerName;
+            // localPlayer.playerColor = lobby.playerColor;
         }
     }
 
