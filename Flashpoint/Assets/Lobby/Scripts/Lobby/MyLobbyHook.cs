3f2a2d99df86aea2a968d3c816bb0a4afc337d4e
pripri99 <raudreymomo@yahoo.fr>
2019-04-07 14:26:15 -0400

    * Put back LobbyHook to original state * Created MyLobbyHook and added it ot FamilyLobby and Veteran Lobby

---
+++ Flashpoint/Assets/Lobby/Scripts/Lobby/MyLobbyHook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Prototype.NetworkLobby
+{
+    public class MyLobbyHook : LobbyHook
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
+        {
+            //gamePlayer.GetComponent<Renderer>().material.color = lobbyPlayer.GetComponent<LobbyPlayer>().playerColor;
+            LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
+            SetupLocalPlayer localPlayer = gamePlayer.GetComponent<SetupLocalPlayer>();
+
+            localPlayer.pname = lobby.playerName;
+            localPlayer.playerColor = lobby.playerColor;
+        }
+    }
+
+}
+
