e30fa2cc850cbf3d26041d7bfc4861516abddb3e
alexywang <alex.yuanda.wang@gmail.com>
2019-04-08 12:56:35 -0400

    no message

---
+++ Flashpoint/Assets/Scripts/PickUpPOI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpPOI : MonoBehaviour
+{
+   
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "POI")
+        {
+            col.transform.parent = gameObject.transform;
+            Debug.Log("Collided: " + col.transform.name);
+
+        }
+
+        if (col.gameObject.tag == "FalseAlarm")
+        {
+            Destroy(col.gameObject);
+
+        }
+
+
+    }
+
+
+}
