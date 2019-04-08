5e788d9d6bbec4af3b49703039af23120220207d
deletestvn <deletestvn@gmail.com>
2019-04-08 12:41:00 -0400

    add changes to firefighters, spwan works, turn works

--- Flashpoint/Assets/Scripts/Firefighter/FirefighterTouchDoor.cs
+++ Flashpoint/Assets/Scripts/Firefighter/FirefighterTouchDoor.cs
@@ -11,14 +11,23 @@ public class FirefighterTouchDoor : MonoBehaviour
     private DoorController m_TargetDoor;    // Reference of the selected Door
     private Transform m_Transform;          // Reference used to turn the firefighter
 
+    private bool m_DoorState;               // open or not open
+
     // Set Method
     public void SetTarget(DoorController TargetDoor)
     {
         m_TargetDoor = TargetDoor;
+        m_DoorState = m_TargetDoor.getDoorState();
     }
 
     // Get Method
     public Animator get_m_Animator() { return this.m_Animator; }
+
+    void Awake()
+    {
+        m_Transform = GetComponent<Transform>();
+        m_Animator = GetComponent<Animator>();
+    }
 
     // Turn Method: rotate the firefighter when not facing the target door
     public void Turn(float turnInput)
@@ -27,15 +36,46 @@ public class FirefighterTouchDoor : MonoBehaviour
         m_Transform.rotation = Quaternion.Lerp(m_Transform.rotation, turnRotation, m_TurnSpeed * Time.deltaTime);
     }
 
+    // Action
     public void TouchDoor()
     {
         m_Animator.SetTrigger("TouchDoor");
+        if (m_TargetDoor.gameObject.transform.localPosition.x > m_Transform.localPosition.x)
+        {
+            Turn(90f);
+        }
+
+        if (m_TargetDoor.gameObject.transform.localPosition.x < m_Transform.localPosition.x)
+        {
+            Turn(-90f);
+        }
+
+        if (m_TargetDoor.gameObject.transform.localPosition.z > m_Transform.localPosition.z)
+        {
+            Turn(0f);
+        }
+
+        if (m_TargetDoor.gameObject.transform.localPosition.z < m_Transform.localPosition.z)
+        {
+            Turn(180f);
+        }
+        m_TargetDoor.InteractDoor();
+        //StartCoroutine(TouchDoorRoutine()); ;
+    }
+
+    IEnumerator TouchDoorRoutine()
+    {
+        m_Animator.SetTrigger("TouchDoor");
+        yield return new WaitForSeconds(2f);
         m_TargetDoor.InteractDoor();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool DoorTouched()
     {
-        
+        if (m_DoorState != m_TargetDoor.getDoorState())
+        {
+            return true;
+        }
+        else return false;
     }
 }
--- Flashpoint/Assets/Scripts/Firefighter/Game.cs
+++ Flashpoint/Assets/Scripts/Firefighter/Game.cs
@@ -8,75 +8,33 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public FirefighterManager[] m_Firefighters;     // A collection of managers for enabling and disabling different aspects of the Firefighter
+    public FirefighterManager m_Firefighter;
 
-    //public CameraView m_CameraControl;
-    //public GameObject m_FirefighterPrefab;          // Reference to the prefab the players will control
-    //public FirefighterManager[] m_Firefighters;     // A collection of managers for enabling and disabling different aspects of the Firefighter
-    public FirefighterManager firefighter;
-    // Start is called before the first frame update
+    private bool m_hasLevelStarted = false;
+    private bool m_isGamePlaying = false;
+    private bool m_isGameOver = false;
+    private bool m_hasLevelFinished = false;
+
+    public bool HasLevelStarted { get { return m_hasLevelStarted; } set { m_hasLevelStarted = value; } }
+    public bool IsGamePlaying { get { return m_isGamePlaying; } set { m_isGamePlaying = value; } }
+    public bool IsGameOver { get { return m_isGameOver; } set { m_isGameOver = value; } }
+    public bool HasLevelFinished { get { return m_hasLevelFinished; } set { m_hasLevelFinished = value; } }
+
+    private WaitForSeconds m_StartWait = new WaitForSeconds(1f);
+    private WaitForSeconds m_EndWait = new WaitForSeconds(1f);
+
+    void Awake()
+    {
+        m_Firefighters = FindObjectsOfType<FirefighterManager>();
+        m_Firefighter = m_Firefighters[0];
+    }
+
     void Start()
     {
-        //SpawnAllFirefighters();
-        Vector2Int gridPoint = Geometry.GridPoint(0, 0);
-        Vector3 point = Geometry.PointFromGrid(gridPoint);
-        firefighter = FindObjectOfType<FirefighterManager>();
-        //StartCoroutine(GameLoop());
+        StartCoroutine(GameLoop());
     }
 
-    void Update()
-    {
-        //MouseOverLocation();
-
-        /*if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit) && (hit.transform.gameObject.tag == "InsideTile"  || hit.transform.gameObject.tag == "OutsideTile"))
-            {
-                firefighter.SetTargetTile(hit.transform.gameObject.GetComponent<Selectable>());
-                firefighter.EnableMove();
-            }
-
-            else if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.tag == "Wall")
-            {
-                firefighter.SetTargetWall(hit.transform.gameObject.GetComponent<WallController>());
-                firefighter.EnablePunch();
-            }
-        }*/
-
-
-        
-        
-
-    }
-
-    //Test action menu
-    public void Move(Selectable target)
-    {
-        firefighter.SetTargetTile(target);
-        firefighter.EnableMove();
-
-    }
-
-    public void Chop(WallController target)
-    {
-        firefighter.SetTargetWall(target);
-        firefighter.EnablePunch();
-
-    }
-
-    /*
-    private void SpawnAllFirefighters()
-    {
-        for(int i = 0; i < m_Firefighters.Length; i++)
-        {
-            m_Firefighters[i].m_Instance = Instantiate(m_FirefighterPrefab, m_Firefighters[i].m_SpawnPoint.position, m_Firefighters[i].m_SpawnPoint.rotation) as GameObject;
-            m_Firefighters[i].m_PlayerNumber = i + 1;
-            m_Firefighters[i].Setup();
-        }
-    }
-    
     private IEnumerator GameLoop()
     {
         yield return StartCoroutine(RoundStarting());
@@ -86,29 +44,123 @@ public class Game : MonoBehaviour
 
     private IEnumerator RoundStarting()
     {
+        DisableFirefighterControl();
+        while (!FirefighterAllSpawned())
+        {
+            foreach (FirefighterManager firefighter in m_Firefighters)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    RaycastHit hit;
 
+                    if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.tag == "OutsideTile")
+                    {
+                        firefighter.Spawn(hit.transform.gameObject.transform.position);
+                        firefighter.isSpawned = true;
+                    }
+                }
+            }
+            yield return null;
+        }
+        yield return m_StartWait;
     }
 
     private IEnumerator RoundPlaying()
     {
+        m_isGamePlaying = true;
+        while (!m_isGameOver)
+        {
+            foreach (FirefighterManager firefighter in m_Firefighters)
+            {
+                firefighter.EnableAction();
+                firefighter.EnableControl();
 
+                yield return StartCoroutine(TurnPlaying(firefighter));
+                firefighter.Reset();
+            }
+
+            yield return null;
+        }
     }
 
     private IEnumerator RoundEnding()
     {
-
+        Debug.Log("Firefighter Disabled");
+        DisableFirefighterControl();
+        yield return m_EndWait;
     }
-    */
-    private void MouseOverLocation()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
 
-            Vector3 point = hit.point;
-            Vector2Int gridPoint = Geometry.GridFromPoint(point);
-            //Debug.Log(gridPoint);
+    private IEnumerator TurnPlaying(FirefighterManager firefighter)
+    {
+        while (firefighter.getAP() != 0 && firefighter.IsMyTurn())
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit) && (hit.transform.gameObject.tag == "InsideTile" || hit.transform.gameObject.tag == "OutsideTile"))
+                {
+                    //firefighter.SetTargetTile(hit.transform.gameObject.GetComponent<Selectable>());
+                    firefighter.SetTargetSpace(hit.transform.gameObject.GetComponent<Space>());
+                    firefighter.EnableMove();
+                }
+
+                else if (Physics.Raycast(ray, out hit) && hit.transform.gameObject.tag == "Wall")
+                {
+                    firefighter.SetTargetWall(hit.transform.gameObject.GetComponent<WallController>());
+                    firefighter.EnablePunch();
+                }
+
+                else if (Physics.Raycast(ray, out hit) && (hit.transform.gameObject.tag == "DoorInside" || hit.transform.gameObject.tag == "DoorOutside"))
+                {
+                    firefighter.SetTargetDoor(hit.transform.gameObject.GetComponent<DoorController>());
+                    firefighter.EnableTouchDoor();
+                }
+            }
+            Debug.Log("Firefighter No." + firefighter.m_PlayerNumber + " AP: " + firefighter.getAP());
+            yield return null;
         }
+    }
+
+    private void DisableFirefighterControl()
+    {
+        foreach(FirefighterManager firefighter in m_Firefighters)
+        {
+            firefighter.DisableControl();
+        }
+    }
+
+    private void EnableFirefighterControl()
+    {
+        foreach (FirefighterManager firefighter in m_Firefighters)
+        {
+            firefighter.EnableControl();
+        }
+    }
+
+    private bool FirefighterAllSpawned()
+    {
+        bool AllSpawned = true;
+        foreach (FirefighterManager firefighter in m_Firefighters)
+        {
+            if (!firefighter.isSpawned) 
+            {
+                AllSpawned = false;
+            }
+        }
+        return AllSpawned;
+    }
+
+    //Test action menu
+    public void Move(Selectable target)
+    {
+        m_Firefighter.EnableMove();
+    }
+
+    public void Chop(WallController target)
+    {
+        m_Firefighter.EnablePunch();
     }
 }
